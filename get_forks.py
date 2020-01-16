f19e672155e56fecb4900c08dbf294d915493419
Jerry Liu <jerry@Jerryde-MacBook-Pro.local>
2020-01-16 23:12:14 +0800

    ...

---
+++ get_forks.py
@@ -0,0 +1,10 @@
+import requests
+import json
+
+r = requests.get('https://api.github.com/orgs/fedora-infra/repos')
+
+json_dict = json.loads(r.text)
+
+
+for item in json_dict:
+    print(item['forks'])
